6ca27c68eb1416c0a8ac21efa7d4fc642cf047d5
PyrosStuff <119268845+PyrosStuff@users.noreply.github.com>
2023-05-09 12:16:12 -0400

    Update V1.2.01.py

--- V1.1.py
+++ V1.1.py
@@ -41,7 +41,7 @@ async def on_ready():
 
 Current middleman is {client.user}.
 Type (help in Discord chat to get started.
-Version 1.1
+Version 1.2
 
 Eagle-Eye is maintained and coded by https://github.com/PyrosStuff
 
